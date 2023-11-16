a7914f5b88b68fcc14c4241ecade7ec3cb8590d7
venya2201 <benjamin02226@gmail.com>
2023-11-16 16:17:02 +0200

    First commit

---
+++ src/main/java/pages/GetTheApp.java
@@ -0,0 +1,56 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class GetTheApp extends BasePage{
+
+    public GetTheApp(WebDriver driver) {
+        super(driver);
+    }
+    private WebElement iphone(){
+        return findByXpath("//label[@class='label-ios']");
+    }
+    private WebElement android(){
+        return findByXpath("//label[@class='label-android']");
+    }
+    private WebElement notifications(){
+        return findByXpath("//label[@class='checkbox-input']");
+    }
+    private WebElement phone(){
+        return findByXpath("//input[@id='mobile-number']");
+    }
+    private WebElement link(){
+        return findByXpath("//a[@class='btn btn--orange btn-text-link full-width ng-binding']");
+    }
+    private WebElement getTheAppPath(){
+        return findByXpath("//a[@class='how-it-works_link'][text()='Get The App']");
+    }
+
+    public GetTheApp OpenLink(){
+        getTheAppPath().click();
+        return this;
+    }
+
+    public GetTheApp IphoneBtn(){
+        iphone().click();
+        return this;
+    }
+    public GetTheApp AndroidBtn(){
+        android().click();
+        return this;
+    }
+    public GetTheApp ReceiveSMSNotification(){
+        notifications().click();
+        return this;
+    }
+    public GetTheApp PhoneNumber(){
+        phone().click();
+        phone().sendKeys("484 246-7854");
+        return this;
+    }
+    public GetTheApp TextPhoneLink(){
+        link().click();
+        return this;
+    }
+}
---
+++ src/test/java/tests/GetTheAppTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.testng.annotations.Test;
+import pages.GetTheApp;
+
+public class GetTheAppTest extends TestInit{
+
+  @Test
+    public void getApp(){
+      GetTheApp app = new GetTheApp(getDriver());
+    app.scroll(500);
+    app.OpenLink();
+            app.IphoneBtn()
+                .AndroidBtn()
+                .ReceiveSMSNotification()
+                .PhoneNumber()
+                .TextPhoneLink();
+
+    }
+}
