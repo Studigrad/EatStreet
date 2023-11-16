0984cb91474ac9b056d63cb2381e337d6d21c39d
OlenaMyla <njonja1309@gmail.com>
2023-11-16 15:10:14 +0200

    scroll

--- src/main/java/components/Footer.java
+++ src/main/java/components/Footer.java
@@ -1,7 +1,9 @@
 package components;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import pages.BasePage;
 
 public class Footer extends BasePage {
