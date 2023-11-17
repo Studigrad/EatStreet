b8b8aa140faad2af3e035b8d3a6498b8b4ce79e0
Venusichek <venjasqa@gmail.com>
2023-11-17 13:32:02 +0200

    Zalupa

---
+++ src/main/java/pages/footerPages/CustomersPage.java
@@ -0,0 +1,9 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CustomersPage extends BasePage{
+    public CustomersPage(WebDriver driver){
+        super(driver);
+    }
+}
