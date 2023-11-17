2a6a7563431b645ba31edbc06f4800101a96c36b
anufiz <130396920+anufiz@users.noreply.github.com>
2023-11-17 15:25:22 +0200

    add new page

--- src/main/java/pages/Restaurants.java
+++ src/main/java/pages/Restaurants.java
@@ -7,60 +7,19 @@ import java.util.List;
 
 public class Restaurants extends BasePage{
 
-    public Restaurants(WebDriver driver) {
+    public Restaurants(WebDriver driver){
         super(driver);
     }
-
-    //Order for
-    public WebElement deliveryRadioButton(){
-        return findByXpath("//input[@id=\"filters-checkbox-delivery\"]");
+    public List<WebElement> menuList(){
+        return findsByXpath("//div[@class='menu-list ng-scope']");
     }
-
-    public WebElement takeoutRadioButton(){
-        return findByXpath("//input[@id=\"filters-checkbox-takeout\"]");
+    public WebElement backToCity(){
+        return findByXpath("//div[@ng-class='{'hero-unit--express-lunch' : menuCtrl.isExpressLunch(), 'hero-unit--menu-with-reviews' : !menuCtrl.isExpressLunch()}']");
     }
-
-    public WebElement freeDeliveryButton(){
-        return findByXpath("//label[@for=\"filter-Free Delivery\"]");
+    public WebElement searchMenuField(){
+        return findByXpath("//input[@class='margin-bottom-0 menu-search__input ng-pristine ng-untouched ng-valid ng-empty']");
     }
-
-    public WebElement rating4PlusButton(){
-        return findByXpath("//label[@for=\"filter-Rating 4+\"]");
-    }
-
-    public WebElement orderAheadButton(){
-        return findByXpath("//label[@for=\"filter-Order Ahead\"]");
-    }
-    //Categories
-    public List<WebElement> categories(){
-        return findsByXpath("//div[@class=\"row filters-section\"]");
-    }
-    //Sort by (Button)
-    public WebElement sortByButton(){
-        return findByXpath("//div[@es-container=\"sortByRestaurants\"]");
-    }
-    //Sort by elements
-    public WebElement alphabeticalSort(){
-        return findByXpath("//button[@aria-label=\"alphabetical\"]");
-    }
-    public WebElement etaSort(){
-        return findByXpath("//button[@aria-label=\"eta\"]");
-    }
-    public WebElement distanceSort(){
-        return findByXpath("//button[@aria-label=\"distance\"]");
-    }
-    public WebElement ratingSort(){
-        return findByXpath("//button[@aria-label=\"rating\"]");
-    }
-    public WebElement deliveryFeeSort(){
-        return findByXpath("//button[@aria-label=\"delivery_fee\"]");
-    }
-    //All restaurants list
-    public List<WebElement> allRests(){
-        return findsByXpath("//ul[@id=\"all-rests\"]");
-    }
-    //View more
-    public WebElement loadMoreRestaurants(){
-        return findByXpath("//button[@ng-click=\"$ctrl.viewMoreRestaurants()\"]");
+    public WebElement upScopeSomeCategory(){
+        return findByXpath("//span[@class='angle-up ng-scope']");
     }
 }
