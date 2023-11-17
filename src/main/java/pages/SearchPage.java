2c0c2a1d50bad2077e9ca1b266519b119a61dab3
Studigrad <studigradilya@gmail.com>
2023-11-17 16:59:01 +0200

    Add new test(fixed)

--- src/main/java/pages/SearchPage.java
+++ src/main/java/pages/SearchPage.java
@@ -6,12 +6,8 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 
 public class SearchPage extends BasePage{
-
     public SearchPage(WebDriver driver) {
         super(driver);
-    }
-    public void clickSortByBtn(){
-        findByXpath("//button[@class=\"custom_dropdown_button\"]").click();
     }
     private List<WebElement> sortByElementsList(){
         return findsByXpath("//div[@class=\"custom_dropdown_content\"]/button");
@@ -31,15 +27,17 @@ public class SearchPage extends BasePage{
     private List<WebElement> filterSectionList(){
         return findsByXpath("//div[@class=\"row filters-section ng-scope\"]/div");
     }
-    public void clickFilterBtnByIndex(int index){
-        filterSectionList().get(index-1).click();
-    }
-
     public WebElement getRestaurantByIndex(int index){
        return getListOfRestaurants().get(index-1);
     }
     public void clickSortByElementByIndex(int index){
         sortByElementsList().get(index-1).click();
+    }
+    public void clickSortByBtn(){
+        findByXpath("//button[@class=\"custom_dropdown_button\"]").click();
+    }
+    public void clickFilterBtnByIndex(int index){
+        filterSectionList().get(index-1).click();
     }
     public void clickOrderForRadioBtn(int index){
         switch (index) {
