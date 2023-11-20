9262decfb453faf4f366f8af1910e91361abea5c
Rroman82 <Mypost082017@gmail.com>
2023-11-20 10:15:48 +0200

    Fixed

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -12,74 +12,89 @@ import java.util.List;
 public class CartPage extends BasePage {
 
 
-
     public CartPage(WebDriver driver) {
         super(driver);
     }
 
-    public void navigate (String link){driver.get(link);}
-
-    public WebElement getCheckCart(){
-        return  elementByXpath("//div[@title='Click here to view your cart.']");
+    public void navigate(String link) {
+        driver.get(link);
     }
+
+    public WebElement getCheckCart() {
+        return elementByXpath("//div[@title='Click here to view your cart.']");
+    }
+
     public WebElement getCheckCartStatus() {
         return elementByXpath("//p[@class='cart-empty-text font-family-museo-slab font-size-16']");
     }
 
-        public WebElement getCheckDelivery(){
-            return elementByXpath("//div/label[@id='delivery-checkbox']");
-        }
+    public WebElement getCheckDelivery() {
+        return elementByXpath("//div/label[@id='delivery-checkbox']");
+    }
 
-        public WebElement getCheckTakeout(){
+    public WebElement getCheckTakeout() {
         return elementByXpath("//div/label [@id='takeout-checkbox']");
-                }
-                public WebElement getCheckOutOrder(){
+    }
+
+    public WebElement getCheckOutOrder() {
         return elementByXpath("//a[@id='click-checkout']");
-                }
-         public WebElement getEnterOderDataName (){
+    }
+
+    public WebElement getEnterOderDataName() {
         return elementByXpath("//div/input[@id='firstName']");
-         }
+    }
 
-         public WebElement getEnterOrderDataSurname (){
+    public WebElement getEnterOrderDataSurname() {
         return elementByXpath("//div/input[@name='lastName']");
-         }
-         public WebElement getenterOrderDataPhoneNum(){
-        return elementByXpath("//div/input[@name='phoneNumber']");
-         }
-public WebElement getEnterOrderDataAPT(){
-        return elementByXpath("//div/input[@name='aptNumber']");
-}
-public WebElement getenterOrderDataBilding(){
-        return elementByXpath("//div/input[@name='buildingName']");
-}
-public WebElement getclickTakeoutBtn(){
-        return elementByXpath("//div/label[@title='Click here to order takeout.']");
-}
-public WebElement getclickDeliveryBtn(){
-        return elementByXpath("//div/label[@id='radio-delivery-btn']");
-}
-public WebElement getchooseTimeDelivery(){
-        return elementByXpath("//div/select[@class='time-select ng-pristine ng-touched']");
-}
+    }
 
-public WebElement getEnterOrderInstruction(){
+    public WebElement getenterOrderDataPhoneNum() {
+        return elementByXpath("//div/input[@name='phoneNumber']");
+    }
+
+    public WebElement getEnterOrderDataAPT() {
+        return elementByXpath("//div/input[@name='aptNumber']");
+    }
+
+    public WebElement getenterOrderDataBilding() {
+        return elementByXpath("//div/input[@name='buildingName']");
+    }
+
+    public WebElement getclickTakeoutBtn() {
+        return elementByXpath("//div/label[@title='Click here to order takeout.']");
+    }
+
+    public WebElement getclickDeliveryBtn() {
+        return elementByXpath("//div/label[@id='radio-delivery-btn']");
+    }
+
+    public WebElement getchooseTimeDelivery() {
+        return elementByXpath("//div/select[@class='time-select ng-pristine ng-touched']");
+    }
+
+    public WebElement getEnterOrderInstruction() {
         return elementByXpath("//div/label/textarea[@name='comments']");
-}
-public WebElement getClickBackToMenuBtn(){
+    }
+
+    public WebElement getClickBackToMenuBtn() {
         return elementByXpath("//div/a[@class='btn btn-primary float-left']");
-}
-public WebElement getClickContinueOrder(){
+    }
+
+    public WebElement getClickContinueOrder() {
         return elementByXpath("//div/button[@id='continue']");
-}
-public WebElement getEnter
-    public WebElement elementByXpath(String locator){
+    }
+
+    //public WebElement getEnter
+    public WebElement elementByXpath(String locator) {
         return findByXpath(locator);// елемент
     }
 
 
-    public WebElement clickOnButton(){
+    public WebElement clickOnButton() {
         return elementByXpath("//A[@class]");
     }
+
+
     //    public List<WebElement> elementsByXpath(String locator){
 //        return findsByXpath(locator);// елементи
 //    }
---
+++ src/test/java/tests/TestCart.java
@@ -0,0 +1,138 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.CartPage;
+import pages.HomePage;
+
+public class TestCart extends TestInit {
+    @Test
+    public void checkCart() {
+        HomePage page = new HomePage(getDriver());
+        page.navigate("https://eatstreet.com/");
+        Assert.assertTrue(page.getCheckCart().isDisplayed());
+
+        page.getCheckCart().click();
+        CartPage cartPage = new CartPage(getDriver());
+        Assert.assertTrue(cartPage.getCheckCartStatus().isDisplayed());
+    }
+
+    @Test
+    public void checkDelivery() {
+        HomePage page = new HomePage(getDriver());
+        page.getCheckCart().click();
+        CartPage cartPage = new CartPage(getDriver());
+        cartPage.getCheckDelivery().click();
+        Assert.assertTrue(cartPage.getCheckDelivery().isDisplayed());
+    }
+
+    @Test
+    public void checkTakeout() {
+        CartPage cartPage = new CartPage(getDriver());
+        cartPage.getCheckTakeout().click();
+        Assert.assertTrue(cartPage.getCheckTakeout().isDisplayed());
+    }
+
+    @Test
+    public void checkOutOrder() {
+        CartPage cartPage = new CartPage(getDriver());
+        cartPage.getCheckOutOrder().click();
+        Assert.assertTrue(cartPage.getCheckOutOrder().isDisplayed());
+    }
+
+    @Test
+    public void enterOderDataName() {
+        CartPage cartPage = new CartPage(getDriver());
+        cartPage.getCheckOutOrder().click();
+        Assert.assertTrue(cartPage.getCheckOutOrder().isDisplayed());
+        cartPage.getEnterOderDataName().sendKeys("Robert");
+    }
+
+    @Test
+    public void enterOrderDataSurname() {
+        CartPage cartPage = new CartPage(getDriver());
+        cartPage.getCheckOutOrder().click();
+        Assert.assertTrue(cartPage.getCheckOutOrder().isDisplayed());
+        cartPage.getEnterOrderDataSurname().sendKeys("Dauni");
+    }
+
+    @Test
+    public void enterOrderDataPhoneNum() {
+        CartPage cartPage = new CartPage(getDriver());
+        cartPage.getCheckOutOrder().click();
+        Assert.assertTrue(cartPage.getCheckOutOrder().isDisplayed());
+        cartPage.getenterOrderDataPhoneNum().sendKeys("+380975958803");
+
+    }
+    @Test
+    public void enterOrderDataAPT() {
+        CartPage cartPage = new CartPage(getDriver());
+        cartPage.getCheckOutOrder().click();
+        Assert.assertTrue(cartPage.getCheckOutOrder().isDisplayed());
+        cartPage.getEnterOrderDataAPT().sendKeys("666");
+    }
+    @Test
+    public void enterOrderDataBilding() {
+        CartPage cartPage = new CartPage(getDriver());
+        cartPage.getCheckOutOrder().click();
+        Assert.assertTrue(cartPage.getCheckOutOrder().isDisplayed());
+        cartPage.getenterOrderDataBilding().sendKeys("666");
+    }
+    @Test
+    public void clickDeliveryBtn() {
+        CartPage cartPage = new CartPage(getDriver());
+        cartPage.getCheckOutOrder().click();
+        Assert.assertTrue(cartPage.getCheckOutOrder().isDisplayed());
+        cartPage.getclickDeliveryBtn().click();
+        Assert.assertTrue(cartPage.getclickDeliveryBtn().isDisplayed());
+    }
+    @Test
+    public void clickTakeoutBtn() {
+        CartPage cartPage = new CartPage(getDriver());
+        cartPage.getCheckOutOrder().click();
+        Assert.assertTrue(cartPage.getCheckOutOrder().isDisplayed());
+        cartPage.getclickTakeoutBtn().click();
+        Assert.assertTrue(cartPage.getclickTakeoutBtn().isDisplayed());
+    }
+    @Test
+    public void chooseTimeDelivery() {
+        CartPage cartPage = new CartPage(getDriver());
+        cartPage.getCheckOutOrder().click();
+        Assert.assertTrue(cartPage.getCheckOutOrder().isDisplayed());
+        cartPage.getchooseTimeDelivery().click();
+        Assert.assertTrue(cartPage.getclickTakeoutBtn().isDisplayed());
+        //дія вибір зі списку конкретного часу
+    }
+    @Test
+    public void enterOrderInstruction() {
+        CartPage cartPage = new CartPage(getDriver());
+        cartPage.getCheckOutOrder().click();
+        Assert.assertTrue(cartPage.getCheckOutOrder().isDisplayed());
+        cartPage.getEnterOrderInstruction().sendKeys("leave at door");
+    }
+    @Test
+    public void clickBackToMenuBtn() {
+        CartPage cartPage = new CartPage(getDriver());
+        cartPage.getCheckOutOrder().click();
+        Assert.assertTrue(cartPage.getCheckOutOrder().isDisplayed());
+        cartPage.getClickBackToMenuBtn().click();
+    }
+    @Test
+    public void clickContinueOrder() {
+        CartPage cartPage = new CartPage(getDriver());
+        cartPage.getCheckOutOrder().click();
+        Assert.assertTrue(cartPage.getCheckOutOrder().isDisplayed());
+        cartPage.getClickContinueOrder().click();
+    }
+
+
+//    public void chooseRestaurant(){
+//        HomePage page = new HomePage(getDriver());
+//        page.navigate(("https://eatstreet.com/"));
+//        Assert.assertTrue(page.findSearchField().isDisplayed());
+//
+//        HomePage HomePage = new HomePage(getDriver());
+//
+//    }
+}
+
